e37c15e15d13237e2dd9e84f0beca04efb5bf802
Erwin Ried <erwin@ried.cl>
2012-09-29 17:20:23 -0300

    Example menu not updating properly
    Quick fix for 1.0.1H (20120930)

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -507,6 +507,7 @@ public class Editor extends JFrame implements RunnerListener {
     fileMenu.add(sketchbookMenu);
 
     if (examplesMenu == null) {
+      examplesMenu = new JMenu(_("Examples"));
       rebuildExamplesMenu();
     }
     fileMenu.add(examplesMenu);
@@ -605,7 +606,6 @@ public class Editor extends JFrame implements RunnerListener {
   }
   
   private void rebuildExamplesMenu(){
-  	  examplesMenu = new JMenu(_("Examples"));
       base.rebuildExamplesMenu(examplesMenu);
 
       int upper = 0, lower = 0;
