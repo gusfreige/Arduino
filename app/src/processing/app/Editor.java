4e2a003da583bee73b487825d2932cdb90203e15
Erwin Ried <erwin@ried.cl>
2012-10-15 18:51:55 -0300

    Version update: 1.0.1I

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -2263,7 +2263,7 @@ public class Editor extends JFrame implements RunnerListener {
     // Set the title of the window to "sketch_070752a - Processing 0126"
     setTitle(
       I18n.format(
-	_("{0}: Arduino ERW {1}h"),
+	_("{0}: Arduino ERW {1}i"),
 	sketch.getName(),
 	Base.VERSION_NAME
       )
--- app/src/processing/app/Preferences.java
+++ app/src/processing/app/Preferences.java
@@ -68,7 +68,7 @@ public class Preferences {
 
   // what to call the feller
 
-  static final String PREFS_FILE = "idesettings_v1.0.e.txt";
+  static final String PREFS_FILE = "idesettings_v1.0.i.txt";
 
 
   // prompt text stuff
@@ -213,7 +213,7 @@ public class Preferences {
     // start by loading the defaults, in case something
     // important was deleted from the user prefs
     try {
-      load(Base.getLibStream("idesettings_v1.0.e.txt"));
+      load(Base.getLibStream("idesettings_v1.0.i.txt"));
     } catch (Exception e) {
       Base.showError(null, _("Could not read default settings.\n" +
                              "You'll need to reinstall Arduino."), e);
