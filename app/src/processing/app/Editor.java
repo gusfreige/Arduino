88d56be6b2a5f374505a9c71c4f5f0a3b4588420
Erwin Ried <erwin@ried.cl>
2013-03-13 18:56:33 -0300

    Fix for Editor copy without selection and cuting something when caret is at the last position in the document
    From https://github.com/energia/Energia/issues/195 and
    https://github.com/energia/Energia/issues/194

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -1773,7 +1773,6 @@ public class Editor extends JFrame implements RunnerListener {
    * Implements Edit &rarr; Cut.
    */
   public void handleCut() {
-  	selectLineIfNeeded();
     textarea.cut();
     sketch.setModified(true);
   }
@@ -1783,7 +1782,6 @@ public class Editor extends JFrame implements RunnerListener {
    * Implements Edit &rarr; Copy.
    */
   public void handleCopy() {
-  	selectLineIfNeeded();
     textarea.copy();
   }
 
@@ -2901,17 +2899,7 @@ public class Editor extends JFrame implements RunnerListener {
       super.show(component, x, y);
     }
   }
-  
-  	// Selects a complete line if nothing is selected
-	private void selectLineIfNeeded()
-	{
-		if (!isSelectionActive()) 
-		{
-		    int lineNo = textarea.getLineOfOffset(textarea.getCaretPosition());
-		    textarea.select(getLineStartOffset(lineNo), getLineStopOffset(lineNo));
-		}
-	}
-  
+
   // Search a JEditTextArea for a selected reference, returning the reference is valid, or null 
   public String searchReference(JEditTextArea jtext)
     {
